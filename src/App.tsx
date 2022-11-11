e419cac2d98a245be1427ecfbe61364ec3eef2ad
Stizel <79175842672@yandex.ru>
2022-11-11 19:33:23 +0300

    Pure TS project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-Hello
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="App">
+            <p>Hello </p>
+        </div>
+    );
 }
 
-export default App;
+
